30411e829f7a4422c2b7cdbc9fa2cd1346d775ea
Philip Thomsen <philip-th@live.dk>
2023-04-03 16:23:08 +0200

    CO2 reduction

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-const eff = 0.20 // 20%
-const cloudcover = 69 / 100 // 46%
-const hoursOfDay = 13
-const hoursOfSun = hoursOfDay - cloudcover * hoursOfDay
-const powerRating = 300 //Watts
+import solarData from "./sun.json"
+let eff = 0.20 // 20%
+let cloudcover = 12 / 100 // 46%
+let hoursOfDay = 13
+let hoursOfSun = hoursOfDay - cloudcover * hoursOfDay
+let powerRating = solarData[5].Antal_solceller * solarData[5].kapasitet_pr_panel_i_Wh //Watts
+
+let energyProduction = calculateSolarEnergyProduced(powerRating, hoursOfSun, eff); // in watt-hours
+let carbonIntensity = 0.5; // in kg CO2 per kWh
+
+let co2Reduction = calculateCO2Reduction(energyProduction, carbonIntensity);
 
 console.log(hoursOfSun);
 function calculateSolarEnergyProduced(power, hoursOfSunlight, efficiency) {
@@ -18,6 +24,16 @@ function calculateSolarEnergyProduced(power, hoursOfSunlight, efficiency) {
   return energyProduced;
 }
 
+function calculateCO2Reduction(energyProduction, carbonIntensity) {
+  // Convert energy production to kilowatt-hours
+  energyProduction = energyProduction / 1000;
+  
+  // Calculate CO2 reduction in kilograms
+  let co2Reduction = energyProduction * carbonIntensity;
+  
+  return co2Reduction;
+}
+console.log("CO2 reduction: " + co2Reduction + " kg");
 console.log(calculateSolarEnergyProduced(powerRating, hoursOfSun, eff),"Wh");
 function App() {
   return (
@@ -25,6 +41,7 @@ function App() {
       <h1>Production: {calculateSolarEnergyProduced(powerRating, hoursOfSun, eff).toFixed(1)} Wh på 13 timer</h1>
       <h2>Skydække: {(cloudcover * 100).toFixed(0)}%</h2>
       <h2>Timers sol: {hoursOfSun.toFixed(1)}</h2>
+      <h2>CO2 reduction: {calculateCO2Reduction(energyProduction, carbonIntensity).toFixed(1)} kg</h2>
     </div>
   );
 }
