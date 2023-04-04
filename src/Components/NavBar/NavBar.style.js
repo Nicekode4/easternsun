c427c63bfbde869e0d1cba18dcf48b6cb341faa3
Philip Thomsen <philip-th@live.dk>
2023-04-04 10:53:24 +0200

    NavBar styling

--- src/Components/NavBar/NavBar.style.js
+++ src/Components/NavBar/NavBar.style.js
@@ -5,21 +5,31 @@ display: grid;
 grid-template-columns: repeat(4, 1fr);
 position: fixed;
 bottom: 0;
-padding-bottom: 1rem;
-background-color: black;
-width: 100%;
+padding-bottom: 0.5rem;
+padding-top: 0.5rem;
+background-color: gray;
+width: 22rem;
+border-radius: 20px;
+margin: 0.5rem;
+text-align: center;
+a{
+    width: 2rem;
+    padding: 0.3rem;
+}
 div{
-    padding-top: 0.5rem;
+    padding-top: 0.7rem;
     a{
-        
+        padding: 0.5rem;
           text-decoration: none;
     color: white;  
     }
 }
 img{
     height: 3vh;
+    
 }
 .active{
-    background-color: red;
+    border-radius: 50px;
+    background-color: black;
 }
 `
--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import logo from '../../Images/logo-no-background.png'
+import list from '../../Images/list.png'
+import compass from '../../Images/compass.png'
+import calc from '../../Images/calc.png'
 import { NavLink } from 'react-router-dom'
 import { NavBarStyle } from './NavBar.style'
 
@@ -7,9 +10,9 @@ function Navbar() {
   return (
     <NavBarStyle>
     <div><NavLink to="/">Home</NavLink></div>
-    <NavLink to="/summary"><img src={logo} alt="logo" /></NavLink>
-    <NavLink to="/summary"><img src={logo} alt="logo" /></NavLink>
-    <NavLink to="/summary"><img src={logo} alt="logo" /></NavLink>
+    <NavLink to="/summary"><img src={list} alt="logo" /></NavLink>
+    <NavLink to="/1"><img src={compass} alt="logo" /></NavLink>
+    <NavLink to="/2"><img src={calc} alt="logo" /></NavLink>
     </NavBarStyle>
     
   )
