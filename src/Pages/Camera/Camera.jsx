b9d856c0cda41d0c000d71e30a768a612822324b
Philip Thomsen <philip-th@live.dk>
2023-04-18 20:37:32 +0200

    Update Camera.jsx

--- src/Pages/Camera/Camera.jsx
+++ src/Pages/Camera/Camera.jsx
@@ -5,6 +5,8 @@ import Video from './Video';
 
 const Camera = () => {
     const [output, setOutput] = useState(null)
+    const [value, setValue] = useState(null)
+
     function calculateSolarPanelPowerProduction(tiltAngle, azimuthAngle, panelCapacity) {
         // convert tilt angle and azimuth angle to radians
         const tiltAngleInRadians = tiltAngle * Math.PI / 180;
@@ -21,11 +23,15 @@ const Camera = () => {
         setOutput(powerProduction)
         return powerProduction;
       }
-      
+function handleChange(e) {
+    console.log(e);
+    setValue(300 * e.target.value)
+}      
 function savedAngle(params) {
     
-    calculateSolarPanelPowerProduction(window.localStorage.getItem("myAngle") ? window.localStorage.getItem("myAngle") : 10 , 75, 300)
+    calculateSolarPanelPowerProduction(window.localStorage.getItem("myAngle") ? window.localStorage.getItem("myAngle") : 10 , 75, value)
 }
+
   return (
     <div>
         <Video />
@@ -38,7 +44,15 @@ function savedAngle(params) {
             top: "2rem",
             }}></div>
         <AngleComponent />
-        <h2>Solcellen vil her lave {output} W</h2>
+        <h2>Anlægget vil her lave {output} W</h2>
+        <label htmlFor="paneler"></label>
+        <select onChange={handleChange} name="paneler" id="paneler">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select>
         <button onClick={savedAngle}>leeeeeeed</button>
         
       
