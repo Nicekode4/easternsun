c21bdd09fc1e842a5f8ec74ac35d1141bf54528e
Philip Thomsen <philip-th@live.dk>
2023-04-18 19:47:11 +0200

    Update Camera.jsx

--- src/Pages/Camera/Camera.jsx
+++ src/Pages/Camera/Camera.jsx
@@ -4,8 +4,27 @@ import AngleComponent from './Angle';
 import Video from './Video';
 
 const Camera = () => {
+    const [output, setOutput] = useState(null)
+    function calculateSolarPanelPowerProduction(tiltAngle, azimuthAngle, panelCapacity) {
+        // convert tilt angle and azimuth angle to radians
+        const tiltAngleInRadians = tiltAngle * Math.PI / 180;
+        const azimuthAngleInRadians = azimuthAngle * Math.PI / 180;
+        
+        // calculate the cosine of the tilt angle
+        const tiltCosine = Math.cos(tiltAngleInRadians);
+        
+        // calculate the angle of incidence
+        const angleOfIncidence = Math.acos(tiltCosine * Math.cos(azimuthAngleInRadians));
+        
+        // calculate the power production
+        const powerProduction = panelCapacity * tiltCosine * Math.cos(angleOfIncidence);
+        setOutput(powerProduction)
+        return powerProduction;
+      }
+      
 function savedAngle(params) {
-    alert('saved angle: ' + window.localStorage.getItem('myAngle'))
+    
+    calculateSolarPanelPowerProduction(window.localStorage.getItem("myAngle") ? window.localStorage.getItem("myAngle") : 10 , 75, 300)
 }
   return (
     <div>
@@ -19,6 +38,7 @@ function savedAngle(params) {
             top: "2rem",
             }}></div>
         <AngleComponent />
+        <h2>Solcellen vil her lave {output} W</h2>
         <button onClick={savedAngle}>leeeeeeed</button>
         
       
