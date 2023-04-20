6e578aac083ee2e173ba9d255f473750bdb52956
Philip Thomsen <philip-th@live.dk>
2023-04-20 12:02:05 +0200

    🐛 fix(Map.jsx): fix solarImg assignment conditions ✨ feat(Map.jsx): multiply capacity_pr_panel_in_W with number_of_panels in capacity display The solarImg assignment conditions were incorrect, causing the wrong image to be displayed. The conditions have been fixed to display the correct image based on the capacity of the solar panel. The capacity display has been updated to show the total capacity of the solar panel installation by multiplying the capacity per panel with the number of panels.

--- src/Pages/Map/Map.jsx
+++ src/Pages/Map/Map.jsx
@@ -94,13 +94,13 @@ function MobileMap() {
       <MarkerClusterGroup chunkedLoading>
       {windmillData.map((marker,index) => {
         let solarImg = ""
-        if (marker.capacity_pr_panel_in_W < 1666 && marker.capacity_pr_panel_in_W > 0) {
+        if (marker.capacity_pr_panel_in_W * marker.number_of_panels < 9999 && marker.capacity_pr_panel_in_W * marker.number_of_panels > 0) {
             solarImg = small
         }
-        if (marker.capacity_pr_panel_in_W < 3332 && marker.capacity_pr_panel_in_W > 1667) {
+        if (marker.capacity_pr_panel_in_W * marker.number_of_panels < 25000 && marker.capacity_pr_panel_in_W * marker.number_of_panels > 10000) {
             solarImg = medium
         }
-        if (marker.capacity_pr_panel_in_W < 5000 && marker.capacity_pr_panel_in_W > 3333) {
+        if (marker.capacity_pr_panel_in_W * marker.number_of_panels > 1000000) {
             solarImg = big
         }
          let pos = [
@@ -116,7 +116,7 @@ function MobileMap() {
 
                 <img src={solarImg} alt="" />
             <h2>{marker.address}</h2>
-            <h3>Kapacitet: {marker.capacity_pr_panel_in_W} W</h3>
+            <h3>Kapacitet: {marker.capacity_pr_panel_in_W * marker.number_of_panels} W</h3>
             <h3>Paneler: {marker.number_of_panels}</h3>
             <NavLink to={`/${marker.sid}`}>Se mere</NavLink>
             </Popup>
