adb910dffc5036a8e35f092d8c32eb2d6e8812ca
Philip Thomsen <philip-th@live.dk>
2023-04-30 00:32:54 +0200

    🔒 chore(.gitignore): add .env to ignore list 📦 chore(package.json): add react-dotenv package and update scripts 🌐 chore(public/env.js): add environment variables for API_URL and API_KEY 🔧 chore(FrontPage.jsx, Summary.jsx): use environment variables for API_URL and API_KEY The .env file is now ignored to prevent sensitive information from being committed to the repository. The react-dotenv package is added to allow the use of environment variables in the React application. The start, build, and serve scripts in package.json are updated to include the react-dotenv command. The public/env.js file is added to define the environment variables for API_URL and API_KEY. The FrontPage.jsx and Summary.jsx files are updated to use the environment variables for the API_URL and API_KEY instead of hardcoding them.

--- src/Pages/FrontPage/FrontPage.jsx
+++ src/Pages/FrontPage/FrontPage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import env from "react-dotenv";
 import { NavLink, useParams } from 'react-router-dom'
 import Navbar from '../../Components/NavBar/Navbar'
 import { FrontPageStyle } from './FrontPage.style'
@@ -118,7 +119,7 @@ const [post, setPost] = React.useState(null);
    useEffect(() => {
     const getData = async () => {
       try {
-        const response1 = await axios.get(`https://xdmevphexshiintoioqy.supabase.co/rest/v1/solar?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkbWV2cGhleHNoaWludG9pb3F5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI0MjAzMTMsImV4cCI6MTk5Nzk5NjMxM30.a5P34_o63lHm9HxrPo-0TCYs8udwQBmIBKrKopxKfOQ`);
+        const response1 = await axios.get(`${window.env.API_URL}?apikey=${window.env.API_KEY}`);
         setData(response1.data);
         solarPanelData = response1.data[response1.data?.indexOf(response1.data?.find(c => c.sid == id))]
         const response2 = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${solarPanelData?.Latitude}&longitude=${solarPanelData?.Longtitude}&hourly=temperature_2m,cloudcover&daily=sunrise,sunset&windspeed_unit=ms&timezone=Europe%2FBerlin`);
